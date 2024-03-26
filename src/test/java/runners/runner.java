28cd69a2140515352617a2aa6dec0a35ad747756
AJHG059 <abdulaziz.jameel.alghamdi@gmail.com>
2024-03-26 09:03:52 +0300

    update

--- src/test/java/runners/runner.java
+++ src/test/java/runners/runner.java
@@ -5,19 +5,20 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 // this class run the feauters file
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
         glue = "stapdefinitions",
         features = "src/test/resources/feauters",
-        tags = "@HW2"
+        tags = "@samsung"
 
 
 )
-public class runner {
 // i have 2 tag
 //    1 - @cucmber
 //    2 - @samsung
-//
+public class runner {
+
 
 }
--- src/test/java/stapdefinitions/HomeWork/Day1/HW2.java
+++ src/test/java/stapdefinitions/HomeWork/Day1/HW2.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
@@ -40,19 +42,38 @@ public class HW2 {
     @Then("Click buttons to see message All Buttons Clicked")
     public void click_buttons_to_see_message_all_buttons_clicked() throws InterruptedException {
         // Write code here that turns the phrase above into concrete actions
-        Thread.sleep(4000);
-        WebElement button = driver.findElement(By.id("button00"));
+//        Thread.sleep(4000);
+//        WebElement button = driver.findElement(By.id("button00"));
+//        button.click();
+//        Thread.sleep(5000);
+//        WebElement button1 = driver.findElement(By.id("button01"));
+//        button1.click();
+//        Thread.sleep(5000);
+//        WebElement button2 = driver.findElement(By.id("button02"));
+//        button2.click();
+//        Thread.sleep(5000);
+//        WebElement button3 = driver.findElement(By.id("button03"));
+//        button3.click();
+//        Thread.sleep(5000);
+// ----------------------------------------------------------------------
+// BY USING WEBDRIVER WITE
+// Wait for element with id "button00" to be clickable
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(12));
+        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id("button00")));
         button.click();
-        Thread.sleep(5000);
-        WebElement button1 = driver.findElement(By.id("button01"));
+
+// Wait for element with id "button01" to be clickable
+        WebElement button1 = wait.until(ExpectedConditions.elementToBeClickable(By.id("button01")));
         button1.click();
-        Thread.sleep(5000);
-        WebElement button2 = driver.findElement(By.id("button02"));
+
+// Wait for element with id "button02" to be clickable
+        WebElement button2 = wait.until(ExpectedConditions.elementToBeClickable(By.id("button02")));
         button2.click();
-        Thread.sleep(5000);
-        WebElement button3 = driver.findElement(By.id("button03"));
+
+// Wait for element with id "button03" to be clickable
+        WebElement button3 = wait.until(ExpectedConditions.elementToBeClickable(By.id("button03")));
         button3.click();
-        Thread.sleep(5000);
+
 //        ---------------------------------------------------------------
         Thread.sleep(5000);
         WebElement massage = driver.findElement(By.id("buttonmessage"));
